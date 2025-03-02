3260b877cb8b6f8ddc4b3fc2858d886c0ba42ccd
michalbukalski <michalbukalski@gmail.com>
2025-03-02 21:22:52 +0100

    nowy challange

---
+++ src/ThePrineNumberSecondChallange.java
@@ -0,0 +1,29 @@
+public class ThePrineNumberSecondChallange {
+
+    public static void main(String[] args) {
+
+        int count = 0;
+
+        for (int i = 10; i <= 50; i++) {
+            if(isPrime(i)){
+                //System.out.println("Number " + i + " is a prime number");
+                count++;
+            }
+        }
+        System.out.println("Total number of prime numbers between 10 and 50 is "+ count);
+    }
+
+    public static boolean isPrime(int wholeNumber) {
+
+        if (wholeNumber <= 2) {
+            return (wholeNumber == 2);
+        }
+
+        for (int divisor = 2; divisor < wholeNumber; divisor++) {
+            if (wholeNumber % divisor == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
